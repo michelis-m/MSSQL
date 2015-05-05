7e5113814977512bbad6f809ccfb76e3f426e2b9
michelis-m <michelis_m@yahoo.com>
2015-05-05 15:54:58 +0100

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,20 +1,3 @@
-/****** Script for downloading frames  ******/
-
-SELECT A.frame_id, F.order_id, A.start_date, A.end_date
-
-FROM ECOS_PSI_GOLIVE.txn_data.media_detail_frame A
-
-INNER JOIN ECOS_PSI_GOLIVE.master_data.frame C ON A.frame_id = C.frame_id
-
-INNER JOIN ECOS_PSI_GOLIVE.txn_data.tbl_media_order F ON A.campaign_id = F.campaign_id
-
-
-WHERE F.order_id = 9074;
-
-
-
-****************************************************************************************************
-
 /****** Script for finding columns  ******/
 
 USE ECOS_PSI_GOLIVE
@@ -27,19 +10,7 @@ INNER JOIN sys.columns c
 ON t.object_id = c.object_id
 WHERE c.name like '%line%';
 
-
-****************************************************************************************************
-
-/***** Script for reports since date *****/
-
-SELECT media_detail_id, campaign_id, no_of_frames, media_cost, total_media_cost, media_cost_margin, total_media_cost_margin  
-
-FROM txn_data.media_detail
-
-WHERE start_date > '2015-01-14';
-
-
-*****************************************************************************************************
+/*****************************************************************************************************/
 
 CREATE TRIGGER instead_delete
 ON master_data.frame
@@ -50,7 +21,7 @@ BEGIN
 	SET is_active=0
 	WHERE frame_id IN (SELECT frame_id FROM deleted)
 END
-********************************************************************************************************
+/********************************************************************************************************/
 
 ;WITH table1 as  
 	(
